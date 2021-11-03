f7c02587ddb8ae34bf9fc419a4e86c73be430111
Dima1475 <dima.kolobok.132@mail.ru>
2021-11-03 13:08:15 +0500

    amogus

--- Gemo test application/Applications.xaml.cs
+++ Gemo test application/Applications.xaml.cs
@@ -27,6 +27,7 @@ namespace Gemo_test_application
         Results results;
         GemoTestEntities db = new GemoTestEntities();
         int idWindow = 0;
+        string amogus = "asd";
 
         public Applications(Study study)
         {
